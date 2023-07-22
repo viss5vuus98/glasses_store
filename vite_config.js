6d25f8b2f323df7de1757cb93f4461494719dd1d
viss5vuus98 <dokipu@gmailcom>
2023-07-23 07:29:18 +0800

    環境設置

--- vite_config.js
+++ vite_config.js
@@ -2,10 +2,15 @@
 export default {
   base: '/', //設置Git Pages的根目錄
   assetsDir: 'assets', //設置靜態資源目錄
+  // resolve: {
+  //   alias: {
+  //         '/images': 'src/assets/images',
+  //       },
+  //   },
   // 設定伺服器選項
   server: {
     port: 3000, //設置服務器啟動端口號
-    open: '/index.html', //設置服務器啟動時是否自動打開瀏覽器
+    open: '@/pages/index.html', //設置服務器啟動時是否自動打開瀏覽器
   },
   // 設定構建選項
   build: {
